80712a34cb4a9f63d236176c7315888da29cefe1
Anowar Shajib <ajshajib@uchicago.edu>
2024-04-04 11:53:18 -0500

    Add `test_voronoi_bin` to test_giant.py

--- tests/test_giant.py
+++ tests/test_giant.py
@@ -1,7 +1,9 @@
 import unittest
 import numpy as np
+import numpy.testing as npt
 from squirrel import giant
 from squirrel.data import Spectra
+from squirrel.data import Datacube
 from squirrel.template import Template
 
 
@@ -31,7 +33,53 @@ class TestShoulder(unittest.TestCase):
         giant.Shoulder.log_rebin(self.spectra)
         self.assertIn("log_rebinned", self.spectra.spectra_modifications)
 
-    def test_stand(self):
+    def test_voronoi_bin(self):
+        x = np.arange(11)
+        y = np.arange(11)
+        xx, yy = np.meshgrid(x, y)
+
+        center_pixel_x = 5
+        center_pixel_y = 5
+        coordinate_transform_matrix = np.array([[0.1, 0], [0, 0.1]])
+
+        r = np.sqrt((xx - 5) ** 2 + (yy - 5) ** 2)
+
+        central_snr = 30
+        flux = np.ones((100, 11, 11))
+        flux *= (central_snr**2 / (1 + r))[np.newaxis, :, :]
+        noise = np.sqrt(flux)
+        wavelengths = np.arange(900.0, 1000.0, 1.0)
+        datacube = Datacube(
+            wavelengths,
+            flux,
+            "nm",
+            2.0,
+            0.5,
+            1.0,
+            center_pixel_x,
+            center_pixel_y,
+            coordinate_transform_matrix,
+            noise=noise,
+        )
+        voronoi_binned_spectra = giant.Shoulder.voronoi_bin(
+            datacube, central_snr, 950, 990, 1.0
+        )
+        npt.assert_equal(datacube.wavelengths, voronoi_binned_spectra.wavelengths)
+        self.assertEqual(
+            datacube.wavelength_unit, voronoi_binned_spectra.wavelength_unit
+        )
+        self.assertEqual(datacube.fwhm, voronoi_binned_spectra.fwhm)
+        self.assertEqual(datacube.z_lens, voronoi_binned_spectra.z_lens)
+        self.assertEqual(datacube.z_source, voronoi_binned_spectra.z_source)
+        self.assertEqual(datacube.flux_unit, voronoi_binned_spectra.flux_unit)
+        npt.assert_equal(
+            datacube.spectra_modifications, voronoi_binned_spectra.spectra_modifications
+        )
+        npt.assert_equal(
+            datacube.wavelengths_frame, voronoi_binned_spectra.wavelengths_frame
+        )
+
+    def test_stand_on(self):
         start_wavelength = 9100
         end_wavelength = 9600
         line_mean = 9350
