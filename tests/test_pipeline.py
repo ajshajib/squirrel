1ce23f684a74b7d5c6f3aeeacc6c7b82c3465c7a
Anowar Shajib <ajshajib@uchicago.edu>
2024-05-02 21:42:25 -0500

    Add test for running ppxf on binned spectra in TestPipeline class

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -171,6 +171,62 @@ class TestPipeline:
                 spectra_indices=0,
             )
 
+    def test_run_ppxf_on_binned_spectra(self):
+        start_wavelength = 9100
+        end_wavelength = 9600
+        line_mean = 9350
+        line_sigma = 20
+
+        wavelengths = np.arange(start_wavelength, end_wavelength, 0.5)
+        flux = (
+            -np.exp(-0.5 * (wavelengths - line_mean) ** 2 / line_sigma**2)
+            + (wavelengths - line_mean) / 1000
+        )
+        flux = np.tile(flux, (2, 1)).T
+        noise = np.ones_like(flux) * 0.1
+        fwhm = 0.0
+        spectra = Spectra(wavelengths, flux, "nm", fwhm, 0.5, 2.0, noise=noise)
+
+        template_sigma = 1
+        template_fwhm = 2.355 * template_sigma
+        templates_wavelengths = np.arange(
+            start_wavelength / 1.2, end_wavelength * 1.2, 0.25
+        )
+        template_fluxes = np.random.normal(0, 0.1, (10, len(templates_wavelengths)))
+        template_fluxes[0] = -np.exp(
+            -0.5 * (templates_wavelengths - 9350) ** 2 / template_sigma**2
+        )
+        template_fluxes[1] = -np.exp(
+            -0.5 * (templates_wavelengths - 9450) ** 2 / template_sigma**2
+        )
+
+        template = Template(
+            templates_wavelengths, template_fluxes.T, "AA", template_fwhm
+        )
+
+        Pipeline.log_rebin(spectra)
+
+        velocity_scale_ratio = 2
+        Pipeline.log_rebin(
+            template, velocity_scale=spectra.velocity_scale / velocity_scale_ratio
+        )
+
+        (
+            velocity_dispersion,
+            velocity_dispersion_uncertainty,
+            mean_velocities,
+            mean_velocity_uncertainties,
+        ) = Pipeline.run_ppxf_on_binned_spectra(spectra, template, degree=4)
+
+        input_velocity_dispersion = line_sigma / line_mean * 299792.458
+
+        npt.assert_allclose(
+            velocity_dispersion,
+            [input_velocity_dispersion, input_velocity_dispersion],
+            rtol=0.01,
+            atol=1,
+        )
+
 
 if __name__ == "__main__":
     pytest.main()
