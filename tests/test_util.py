e00b34b727f81921d79917124edafbc219c6facd
Anowar Shajib <ajshajib@uchicago.edu>
2025-03-12 15:20:46 -0500

    Enhance tests for nearest positive definite matrix function with additional cases and tighter tolerance

--- tests/test_util.py
+++ tests/test_util.py
@@ -23,7 +23,19 @@ def test_get_nearest_positive_definite_matrix():
     assert is_positive_definite(nearest_pd_matrix) is True
 
     # Check if the result is close to the original matrix
-    assert np.allclose(nearest_pd_matrix, np.array([[1.5, 1.5], [1.5, 1.5]]), atol=1e-1)
+    assert np.allclose(
+        nearest_pd_matrix, np.array([[1.5, 1.5], [1.5, 1.5]]), atol=1e-10
+    )
+
+    # second matrix
+    matrix = np.array([[0, 0], [0, 1]])
+    nearest_pd_matrix = get_nearest_positive_definite_matrix(matrix)
+
+    # Check if the result is positive-definite
+    assert is_positive_definite(nearest_pd_matrix) is True
+
+    # Check if the result is close to the original matrix
+    assert np.allclose(nearest_pd_matrix, np.array([[0, 0], [0, 1]]), atol=1e-10)
 
 
 if __name__ == "__main__":
